98561eca1d68b0ad3269374b9946aa8f43783298
alfe4r <alfe4r@gmail.com>
2023-07-31 11:54:25 -0400

    Add files via upload
    Relatório de Grupos de Usuários no Active Directory
    Este script PowerShell foi desenvolvido para gerar um relatório dos grupos aos quais os usuários pertencem no Active Directory. Ele utiliza o módulo ActiveDirectory para consultar informações sobre os usuários e grupos e, em seguida, cria um arquivo CSV com os dados coletados.
    
    Funcionalidades:
    Consulta todos os usuários do Active Directory, incluindo a propriedade EmployeeID.
    Para cada usuário, busca os grupos aos quais ele é membro direto ou indireto (por meio de outros grupos).
    Gera um arquivo CSV com as seguintes informações para cada usuário e grupo:
    Nome do usuário
    Nome de login (samAccountName)
    Nome do grupo
    Status do usuário (ativado/desativado)
    DistinguishedName (DN) da Unidade Organizacional (OU) do usuário
    EmployeeID do usuário, se disponível.
    Uso:
    Certifique-se de que o módulo ActiveDirectory esteja instalado no sistema onde o script será executado.
    Execute o script através do PowerShell.
    O relatório será exibido na tela, ordenado por nome de usuário.
    O arquivo CSV será exportado para o diretório "C:\temp" com o nome "GruposAD-DD-MM-YY.csv", onde "DD-MM-YY" representa a data atual no formato dia-mês-ano abreviado.
    Observações:
    Caso o módulo ActiveDirectory não esteja instalado no sistema, o script não funcionará corretamente. Verifique a documentação da Microsoft sobre como instalar o módulo em sistemas Windows.
    O script deve ser executado com privilégios adequados para acessar informações no Active Directory.
    Certifique-se de que o diretório "C:\temp" exista e tenha permissões de gravação para o usuário ou conta de serviço que executará o script.
    Contribuições:
    Este script é fornecido como está, mas sinta-se à vontade para enviar pull requests ou relatar problemas caso encontre alguma melhoria ou correção a ser feita. Vou ficar feliz em revisar e colaborar!
    
    Atenção:
    Este script foi desenvolvido com base em informações disponíveis até a data de corte do meu conhecimento (setembro de 2021). Certifique-se de que o ambiente e os requisitos do Active Directory sejam compatíveis com o script antes de executá-lo em um ambiente de produção.
    
    Licença:
    Este script é fornecido sob a licença MIT. Leia o arquivo LICENSE para obter mais informações sobre os termos da licença.
    
    Agradecimentos:
    Agradeço à comunidade do PowerShell e à Microsoft por fornecerem ferramentas e recursos que tornam o gerenciamento do Active Directory mais fácil e eficiente.
    
    Espero que esta descrição seja útil para documentar o seu script no GitHub. Lembre-se de adaptar as informações conforme necessário, como o nome do arquivo de licença (se estiver usando uma diferente da licença MIT) ou a localização do diretório de saída se estiver usando um caminho diferente de "C:\temp". Boa sorte e continue contribuindo para a comunidade!

---
+++ Exporta-Grupos-Aninhados-do-AD.ps1
@@ -0,0 +1,35 @@
+﻿$relatorio = New-Object System.Data.DataTable
+$relatorio.Columns.Add("Usuario","string") | Out-Null
+$relatorio.Columns.Add("Login","string") | Out-Null
+$relatorio.Columns.Add("Grupo","string") | Out-Null
+$relatorio.Columns.Add("Status","string") | Out-Null
+$relatorio.Columns.Add("OU","string") | Out-Null
+$relatorio.Columns.Add("EmployeeID","string") | Out-Null
+
+$usuarios = Get-ADUser -Filter * -Properties EmployeeID
+
+foreach ($usuario in $usuarios) {
+    $dn = $usuario.DistinguishedName
+    $grupos = Get-ADGroup -LDAPFilter ("(member:1.2.840.113556.1.4.1941:={0})" -f $dn) | Select -ExpandProperty Name
+
+    foreach ($grupo in $grupos) {
+        $linha = $relatorio.NewRow()
+
+        $linha.Usuario = $usuario.Name
+        $linha.Login = $usuario.samAccountName
+        $linha.Grupo = $grupo
+        $linha.Status = $usuario.Enabled
+        $linha.OU = $usuario.distinguishedname
+        $linha.EmployeeID = $usuario.EmployeeID
+
+        $relatorio.Rows.Add($linha)
+    }
+}
+
+$relatorio | Sort Usuario | ft
+
+# Obter a data atual no formato desejado (04-05-23 no exemplo) e adicionar ao nome do arquivo
+$nomeArquivo = "GruposAD-" + (Get-Date -Format "dd-MM-yy") + ".csv"
+$relatorio | Export-Csv -Path "C:\temp\$nomeArquivo" -NoTypeInformation -Encoding UTF8
+
+Write-Host "Relatório exportado para C:\temp\$nomeArquivo"
